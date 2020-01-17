8ddeac19ff6a1e9fa51e0c4919d86205e9db1f82
BengBu-YueZhang <zhangyue_coder@163.com>
2020-01-17 08:39:55 +0800

    测试文件

---
+++ test.js
@@ -0,0 +1,91 @@
+const path = require('path')
+const fs = require('fs')
+const schedule = require('node-schedule')
+const { promisify } = require('util')
+const NewsListModel = require('./model')
+const crawlerGamersky = require('./gamersky')
+const crawlerGameSpot = require('./gamespot')
+const crawlerIGN = require('./ign')
+const { mongoConnect } = require('./config/mongo')
+
+const mode = process.env.mode
+let dataDirPath = mode === 'develop' ? path.resolve(__dirname, './data') : '/var/www/crawlerData'
+const mkdir = promisify(fs.mkdir)
+
+const saveNewsList = async (data = []) => {
+    try {
+        const newsList = new NewsListModel({
+            list: data
+        })
+        await newsList.save()
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const existsDir = async () => {
+    try {
+        if (!fs.existsSync(dataDirPath)) {
+            await mkdir(dataDirPath)
+        } 
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const existsFile = async () => {
+    const date = new Date()
+    const filename = `${date.getFullYear()}-${date.getMonth() + 1}-${date.getDate()}-${date.getHours()}.js`
+    const filePath = path.join(dataDirPath, filename)
+    if (!fs.existsSync(filePath)) {
+        fs.writeFileSync(filePath, '')
+    }
+    return filePath
+}
+
+const crawler = async () => {
+    try {
+        console.log('开始爬取')
+        const [a, b, c] = await Promise.all([
+            crawlerGamersky(),
+            crawlerGameSpot(),
+            crawlerIGN()
+        ])
+        return [...a, ...b, ...c] 
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+const writeData = async (data = []) => {
+    try {
+        const filePath = await existsFile()
+        const writeStream = fs.createWriteStream(filePath)
+        // jsonp
+        writeStream.write(`handleNewsList(${JSON.stringify(data)})`)
+        writeStream.end()
+        writeStream.on('finish', () => {
+            console.error('写入已完成')
+        }) 
+    } catch (error) {
+       console.log(error) 
+    }
+}
+
+const test = async () => {
+    try {
+        await existsDir()
+        const news = await crawler()
+        await saveNewsList(news)
+        writeData(news)
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+
+mongoConnect()
+
+setTimeout(() => {
+    test()
+}, 3000)
